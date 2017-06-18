679f3b5499c9cbe1fd591b4d882383fb7941c0ae
Lars Simkins <lars.simkins@gmail.com>
2017-06-18 14:58:54 -0700

    Singleton instance no longer depends on assignment in Awake

--- DesignLabs_Unity_Examples/Assets/MRDesignLab/HUX/Scripts/Utility/Singleton.cs
+++ DesignLabs_Unity_Examples/Assets/MRDesignLab/HUX/Scripts/Utility/Singleton.cs
@@ -3,24 +3,41 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 //
 using UnityEngine;
-using System.Collections;
 
 namespace HUX.Utility
 {
     public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
-        public static T Instance { get; private set; }
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    T[] instances = FindObjectsOfType<T>();
+                    if (instances.Length == 0)
+                    {
+                        Debug.LogError("No instance of singleton class " + typeof(T) + " found.");
+                    }
+                    else
+                    {
+                        instance = instances[0];
+                        if (instances.Length > 1)
+                        {
+                            Debug.LogError("Multiple instances of singleton class " + typeof(T) + " found.");
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private static T instance;
 
         protected virtual void Awake()
         {
-            if (Instance == null)
-            {
-                Instance = (T)this;
-            }
-            else
-            {
-                Debug.LogError("Got a second instance of the class " + this.GetType());
-            }
+            // Populate on startup
+            T instanceCheck = Instance;
         }
     }
 }
