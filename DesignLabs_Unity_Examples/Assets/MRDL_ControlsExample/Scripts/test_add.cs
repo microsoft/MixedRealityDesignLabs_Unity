85cbbe8d00be925270c354ed91cd37b872bedfce
taylorjames9 <taylorjames9@gmail.com>
2017-07-13 15:28:23 -0700

    test commit

--- DesignLabs_Unity_Examples/Assets/MRDL_ControlsExample/Scripts/test_add.cs
+++ DesignLabs_Unity_Examples/Assets/MRDL_ControlsExample/Scripts/test_add.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class test_add : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+//test code change
 }
